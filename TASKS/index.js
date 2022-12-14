13593096afe9d850bab020ca7914a7cb7ba58e36
yuliabalkiss <balkisula451@gmail.com>
2022-12-14 23:22:35 +0300

    feat: upade file

--- TASKS/index.js
+++ TASKS/index.js
@@ -1,10 +1,36 @@
 // JS Переменные
-let num = 3;
-console.log(num);
+// let num = 3;
+// console.log(num);
 
-let a = 10;
-let b = 2;
-console.log(a + b);
-console.log(a - b);
-console.log(a * b);
-console.log(a / b);
+// let a = 10;
+// let b = 2;
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+
+// let c = 15;
+// let d = 2;
+// let result = c + d;
+// console.log(result);
+
+// let a = 10;
+// let b = 2;
+// let c = 5;
+// console.log(a + b + c);
+
+// let a = 17;
+// let b = 10;
+// let c = (a - b);
+// let d = 7;
+// let result = c + d;
+// console.log(result);
+
+// JSR
+let admin;
+let name;
+name = 'Джон';
+admin = name;
+console.log(admin);
+
+

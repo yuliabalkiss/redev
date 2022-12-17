7402f221d93ea1c145e2cafed0fd821b1a975a8f
yuliabalkiss <balkisula451@gmail.com>
2022-12-17 16:39:39 +0300

    feat: update file

--- TASKS/index.js
+++ TASKS/index.js
@@ -58,5 +58,14 @@
 // " \t \n" - 2
 
 // console.log(10 % 6 === 1 ? true : false);
-let a = 30 % 2;
-console.log(a);
+// let a = 30 % 2;
+// console.log(a);
+// console.log(null == undefined);
+// console.log(null === undefined);
+console.log(5 > 4); //true
+console.log("ананас" > "яблоко");// false
+console.log("2" > "12"); //false
+console.log(undefined == null);//true
+console.log(undefined === null); //false
+console.log(null == "\n0\n");// false
+console.log(null === +"\n0\n");//false

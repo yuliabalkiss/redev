796fa15ff16cc3913a7bbfdb83bcdaba126fbc75
yuliabalkiss <balkisula451@gmail.com>
2022-12-20 23:19:47 +0300

    feat: update file

--- TASKS/index.js
+++ TASKS/index.js
@@ -169,9 +169,23 @@
 //     console.log('Неверно!')
 // }
 
-console.log(5 && 3);
-console.log(0 && 3);
-console.log(5 || 3);
-console.log(0 || 3);
-console.log(0 || 3 && 6);
-console.log(0 && 3 || 6);
+// console.log(5 && 3);
+// console.log(0 && 3);
+// console.log(5 || 3);
+// console.log(0 || 3);
+// console.log(0 || 3 && 6);
+// console.log(0 && 3 || 6);
+
+//===================== JS  Циклы ===============================
+// for (let i = 2; i <= 10; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i)
+//     }
+
+// }
+
+let i = 0;
+while (i < 3) {
+    console.log(`number: ${i}`);
+    i++;
+}
